57e4baeecf23cd83ee3c32ae69acb6681c9f5ba8
Sumedh <sbarsagade248@gmail.com>
2024-04-23 18:36:15 +0530

    New react projects

---
+++ 04tailwaindprops/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-white p-6 mb-10'>Tailwaing Css</h1>
+    <Card channel='Sumedh' />
+    <Card/>
+    </>
+  )
+}
+
+export default App
