2c3d6118d8b03ab58f76a112c2129d55e30c7906
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-08-22 23:08:49 +0530

    new project created using react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React | Sumedh Barsagade</h1>
+      <p>In todays leacture we learn about</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Learn React with React | Sumedh Barsagade</h1>
+    </>
+  )
+}
+
+export default App
