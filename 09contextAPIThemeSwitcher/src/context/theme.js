7f86ab447c2e4dbdafcf9f33546a68ee70f82e7e
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-09-06 17:49:22 +0530

    todo app created using Context API

--- 09contextAPIThemeSwitcher/src/context/theme.js
+++ 09contextAPIThemeSwitcher/src/context/theme.js
@@ -1,5 +1,13 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = React.createContext();
+export const ThemeContext = React.createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
 
-export default ThemeContext
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
