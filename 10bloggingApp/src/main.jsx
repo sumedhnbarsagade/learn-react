4c5795a74ef1da64a2be68672845408b52d8161f
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-09-10 00:01:51 +0530

    BloggingApp

--- 10bloggingApp/src/main.jsx
+++ 10bloggingApp/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import {store} from './/app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <StrictMode>
     <App />
-  </Provider>,
+  </StrictMode>,
 )
--- reduxtoolkittodoapp/src/App.jsx
+++ reduxtoolkittodoapp/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
