7f648dd0861427b15a4c567d1e28e9328c184537
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-09-11 16:45:10 +0530

    connect aplication with redux

--- 10bloggingApp/src/main.jsx
+++ 10bloggingApp/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import React from 'react'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
 )
